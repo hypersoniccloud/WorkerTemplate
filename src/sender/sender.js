574426f0f54dbac5ee47ed09c94448230fac5807
hypersoniccloud <81689387+hypersoniccloud@users.noreply.github.com>
2022-09-22 15:39:56 +0200

    Update sender to save event in mongo

--- src/sender/sender.js
+++ src/sender/sender.js
@@ -1,15 +1,20 @@
-const debug = require('debug')('sender')
-const artemisSender = require('../artemis/artemisSenders')
-const restSender = require('../rest/restSender')
-const config = require('../config')
+const debug = require('debug')('sender');
+const artemisSender = require('../artemis/artemisSenders');
+const restSender = require('../rest/restSender');
+const config = require('../config');
+const mongooseManager = require('../mongo/mongooseManager');
 
 function sendEvent(senderNumber, event) {
+    let response = null;
     if (config.get(`workerTargetSendType${senderNumber}`) === 'SYNC') {
-        restSender.sendEvent(senderNumber, event)
+        response = restSender.sendEvent(senderNumber, event)
     }
     else if (config.get(`workerTargetSendType${senderNumber}`) === 'ASYNC') {
-        artemisSender.sendEvent(senderNumber, event)
+        response = artemisSender.sendEvent(senderNumber, event)
     }
+
+    event.status = response.isInError ? "KO": "OK";
+    mongooseManager.saveEvent(event);        
 }
 
 function startSender(senderNumber) {
